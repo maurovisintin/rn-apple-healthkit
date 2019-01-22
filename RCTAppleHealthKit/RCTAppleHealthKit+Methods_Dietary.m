92179855fa353a88ec3beca3f0ace4aceaac1fc5
MVlaptop <visintin1.mauro@gmail.com>
2019-01-22 17:35:55 +0100

    get water

--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_Dietary.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Dietary.m
@@ -406,4 +406,47 @@
     }];
 }
 
+- (void)water_get:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
+{
+    NSDate *startDate = [RCTAppleHealthKit dateFromOptions:input key:@"startDate" withDefault:nil];
+    NSDate *endDate = [RCTAppleHealthKit dateFromOptions:input key:@"endDate" withDefault:[NSDate date]];
+    HKUnit unit = [RCTAppleHealthKit hkUnitFromOptions:input key:@"unit" withDefault:[HKUnit literUnit]];
+
+    HKQuantityType *waterType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDietaryWater];
+
+    // HKQuantitySample* water = [HKQuantitySample quantitySampleWithType:[HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDietaryWater]
+    //                                                             quantity:[HKQuantity quantityWithUnit:[HKUnit literUnit] doubleValue:waterValue]
+    //                                                             startDate:timeWaterWasConsumed
+    //                                                             endDate:timeWaterWasConsumed
+    //                                                             metadata:nil];
+
+    // Save the water Sample to HealthKit //
+    // [self.healthStore fetchQuantitySamplesOfType:water withCompletion:^(BOOL success, NSError *error) {
+    //     if (!success) {
+    //         NSLog(@"An error occured saving the water sample %@. The error was: ", error);
+    //         callback(@[RCTMakeError(@"An error occured saving the water sample", error, nil)]);
+    //         return;
+    //     }
+    //     callback(@[[NSNull null], @true]);
+    // }];
+
+    NSPredicate * predicate = [RCTAppleHealthKit predicateForSamplesBetweenDates:startDate endDate:endDate];
+
+    [self fetchQuantitySamplesOfType:waterType
+                                unit:unit
+                           predicate:predicate
+                           ascending:ascending
+                               limit:limit
+                          completion:^(NSArray *results, NSError *error) {
+        if(results){
+            callback(@[[NSNull null], results]);
+            return;
+        } else {
+            NSLog(@"error getting water samples: %@", error);
+            callback(@[RCTMakeError(@"error getting water samples", nil, nil)]);
+            return;
+        }
+    }];
+}
+
 @end
